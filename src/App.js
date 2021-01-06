dc580747dc4cb8929bf47735cf580a1f137fc8be
PervertKun <shivenderkumar761@gmail.com>
2021-01-06 11:30:48 +0530

    c

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-import firebase from "./firebase";
+import React, { useState } from "react";
 
 const api = {
   key: "667eaac156fbff26dc85b2c519281d40",
@@ -40,69 +39,59 @@ const dateBuilder = (d) => {
   return `${day} ${date} ${month} ${year}`;
 };
 
-
 function App() {
-useEffect(() => {
-  const messaging = firebase.messaging();
-  messaging.getToken()
-    .then((token) => {
-      console.log("Token : ", token);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-})
-const [query, setQuery] = useState("");
-const [weather, setWeather] = useState({});
 
-const search = (evt) => {
-  if (evt.key === "Enter") {
-    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-      .then((res) => res.json())
-      .then((result) => {
-        setQuery("");
-        setWeather(result);
-        // console.log(result);
-      });
-  }
-};
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
 
-return (
-  <div
-    className={
-      typeof weather.main != "undefined"
-        ? new Date().getHours() >= 20 || new Date().getHours() <= 6
-          ? "App-night"
-          : "App-warm"
-        : "App"
+  const search = (evt) => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setQuery("");
+          setWeather(result);
+          // console.log(result);
+        });
     }
-  >
-    <main>
-      {typeof weather.main != "undefined" ? (
-        <div>
-          <div className="search-bar">
-            <input
-              id="searchBar"
-              type="text"
-              value={query}
-              placeholder="Search your city...."
-              onChange={(e) => setQuery(e.target.value)}
-              onKeyPress={search}
-            />
-          </div>
-          <div className="location-box">
-            <div id="location">
-              {weather.name}, {weather.sys.country}
+  };
+
+  return (
+    <div
+      className={
+        typeof weather.main != "undefined"
+          ? new Date().getHours() >= 20 || new Date().getHours() <= 6
+            ? "App-night"
+            : "App-warm"
+          : "App"
+      }
+    >
+      <main>
+        {typeof weather.main != "undefined" ? (
+          <div>
+            <div className="search-bar">
+              <input
+                id="searchBar"
+                type="text"
+                value={query}
+                placeholder="Search your city...."
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyPress={search}
+              />
             </div>
-            <div id="date">{dateBuilder(new Date())}</div>
-            <div className="weather-box">
-              <div id="temp">{Math.round(weather.main.temp)}°C</div>
-              <br />
-              <div id="weather">{weather.weather[0].main}</div>
+            <div className="location-box">
+              <div id="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div id="date">{dateBuilder(new Date())}</div>
+              <div className="weather-box">
+                <div id="temp">{Math.round(weather.main.temp)}°C</div>
+                <br />
+                <div id="weather">{weather.weather[0].main}</div>
+              </div>
             </div>
           </div>
-        </div>
-      ) : (
+        ) : (
           <div id="no_weather">
             <h1>Enter Your City</h1>
             <input
@@ -116,10 +105,10 @@ return (
             <div id="date1">{dateBuilder(new Date())}</div>
           </div>
         )}
-      <div className="mapping"></div>
-    </main>
-  </div>
-);
+        <div className="mapping"></div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
